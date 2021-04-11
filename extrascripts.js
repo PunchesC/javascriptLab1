3e0ee5a98c806e05a6361b1b7db2942d20071a5a
Curtis <c.b.punches@gmail.com>
2021-04-11 10:10:43 -0400

    Javascript paycheck calculation update

--- extrascripts.js
+++ extrascripts.js
@@ -1,12 +1,15 @@
-const hours = prompt("How many hours did you work this week: ");
-const wage = prompt("What is your wage: ");
+const hours = prompt('How many hours did you work this week: ');
+const wage = prompt('What is your wage: ');
 
 const totalPaycheck = hours * wage;
-console.log(totalPaycheck);
 
-if (hours > 40 ){
-  let bonusPaycheck = 
-  console.log(bonusPaycheck);
+if (hours > 40) {
+  let bonusPaycheck = (hours - 40) * (wage * 0.5) + totalPaycheck;
+  let bonusMessage = 'Your pay with overtime is: ' + '$' + bonusPaycheck;
+  console.log(bonusMessage);
+  alert(bonusMessage);
+} else {
+  let paycheckMessage = 'Your pay is: ' + '$' + totalPaycheck;
+  console.log(paycheckMessage);
+  alert(paycheckMessage);
 }
-// Extra hours is what calculated!!!!! NEED TO BE WORK
-//maybe let cause totalPaycheck may change??????
